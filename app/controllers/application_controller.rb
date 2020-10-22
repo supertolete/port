c79185fe11ad6723d5347bb8121fd8310d58cba5
supertolete <64980770+supertolete@users.noreply.github.com>
2020-10-22 13:42:57 -0400

    Add files via upload

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+ 
+
+http_basic_authenticate_with name: "orlando", password: "lajara" 
+
+ protect_from_forgery with: :exception
+
+
+
+end
---
+++ app/models/idea.rb
@@ -0,0 +1,31 @@
+class Idea < ApplicationRecord
+mount_uploader :picture, PictureUploader
+ validates :name, presence: true,
+                    length: { minimum: 5 }
+
+
+
+
+
+ 
+
+validates :picture,  presence: true,  
+ 
+ 
+
+
+       format: {
+  with: %r{\.gif|jpg|png}i, size_range: 1..3.megabytes,
+  message: 'file must be formats: gif, jpg, or png. Try again'
+}
+
+validate :picture_size_validation, :if => "picture?" 
+ def picture_size_validation
+    errors[:picture] << "should be less than 3MB" if picture.size > 3.megabytes
+  end
+ 
+
+
+
+
+end
